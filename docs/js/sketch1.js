a80d3deb71a2465d16650198c6c0295a60ae4c34
Shutaro Masuoka <56290225+sh-masuoka@users.noreply.github.com>
2021-07-14 21:46:49 +0900

    めっっっっっっっっちゃ追加

--- docs/js/sketch1.js
+++ docs/js/sketch1.js
@@ -1,7 +1,7 @@
 var stop_y = 0;
-var y = 4000;
+var y = 200000;
 var i = 0;
-var step = 2;
+var step = 100;
 var video = document.getElementById('mov');
 
 function Wscroll () {
@@ -10,7 +10,7 @@ function Wscroll () {
         window.scroll (0,y);
         setTimeout ("Wscroll()",10);
         console.log(y);
-        var videoscroll = 86*y/4000;
+        var videoscroll = 86*y/20000;
         console.log(videoscroll);
         video.currentTime = videoscroll;
     }
