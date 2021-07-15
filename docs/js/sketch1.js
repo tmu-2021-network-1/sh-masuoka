1822584673391357f6a9f2577f473c4d4324d8f8
Shutaro Masuoka <56290225+sh-masuoka@users.noreply.github.com>
2021-07-15 19:47:17 +0900

    見栄え

--- docs/js/sketch1.js
+++ docs/js/sketch1.js
@@ -2,9 +2,6 @@ var stop_y = 0;
 var y = 400000;
 var i = 0;
 var step = 60;
-/*
-var video = document.getElementById('mov');
-*/
 console.log(y);
 
 function Wscroll () {
@@ -13,19 +10,7 @@ function Wscroll () {
         window.scroll (0,y);
         setTimeout ("Wscroll()",10);
         
-        /*
-        var videoscroll = 86*y/20000;
-        console.log(videoscroll);
-        video.currentTime = videoscroll;
-        */
     }
-/*
-    if(y==30000000){
-        y=0;
-        window.scroll (0,y);
-        Wscroll();
-    }
-    */
     
 }
 
@@ -35,12 +20,3 @@ function stopScroll(){
     window.location.reload();
 }
  
-/*
-function reScroll(){
-  y=30000000;
-}
-
-function reScroll_2(){
-    y=1;
-  }
-  */
