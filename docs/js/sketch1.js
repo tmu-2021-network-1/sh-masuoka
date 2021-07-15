bf1feb38b4beeeac0c32c75b05d5824b32c1e546
Shutaro Masuoka <56290225+sh-masuoka@users.noreply.github.com>
2021-07-15 16:22:24 +0900

    色々整えています

--- docs/js/sketch1.js
+++ docs/js/sketch1.js
@@ -1,8 +1,10 @@
 var stop_y = 0;
-var y = 400000;
+var y = 360000;
 var i = 0;
-var step = 100;
+var step = 50;
+/*
 var video = document.getElementById('mov');
+*/
 
 function Wscroll () {
     if ( y > stop_y ) {
@@ -10,9 +12,11 @@ function Wscroll () {
         window.scroll (0,y);
         setTimeout ("Wscroll()",10);
         console.log(y);
+        /*
         var videoscroll = 86*y/20000;
         console.log(videoscroll);
         video.currentTime = videoscroll;
+        */
     }
     else{
 
